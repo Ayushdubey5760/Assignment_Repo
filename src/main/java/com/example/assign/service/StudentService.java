5a552b03c46d49792ada8c393a4e49cba5cf4de1
Ayushdubey5760 <75221675+Ayushdubey5760@users.noreply.github.com>
2023-12-12 01:01:32 +0530

    Update StudentService.java

--- src/main/java/com/example/assign/service/StudentService.java
+++ src/main/java/com/example/assign/service/StudentService.java
@@ -1,5 +1,32 @@
-package com.example.assign.service;
+package com.example.assign.domain;
 
-public class StudentService {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String studentname;
+    private String course;
+    private int fee;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(Long id, String studentname, String course, int fee) {
+		super();
+		this.id = id;
+		this.studentname = studentname;
+		this.course = course;
+		this.fee = fee;
+	}
+    
+    
 
 }
+
