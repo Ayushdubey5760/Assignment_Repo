30121e766cf7c9f854a2dd287069d9be313a574c
Ayushdubey5760 <75221675+Ayushdubey5760@users.noreply.github.com>
2023-12-12 00:58:02 +0530

    Update StudentRepository.java

--- src/main/java/com/example/assign/repository/StudentRepository.java
+++ src/main/java/com/example/assign/repository/StudentRepository.java
@@ -1,5 +1,14 @@
 package com.example.assign.repository;
 
-public class StudentRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.assign.service.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+	List<Student> findAll();
 }
